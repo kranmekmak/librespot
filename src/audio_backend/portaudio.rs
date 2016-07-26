ba175580d9ace8e54e832df871521b68941e3484
herrernst <herr.ernst@gmail.com>
2016-07-26 23:18:39 +0200

    close stream on stop

--- src/audio_backend/portaudio.rs
+++ src/audio_backend/portaudio.rs
@@ -2,33 +2,36 @@ use super::{Open, Sink};
 use std::io;
 use portaudio;
 
-pub struct PortAudioSink<'a>(portaudio::stream::Stream<'a, i16, i16>);
+pub struct PortAudioSink<'a>(Option<portaudio::stream::Stream<'a, i16, i16>>);
 
 impl <'a> Open for PortAudioSink<'a> {
     fn open() -> PortAudioSink<'a> {
         portaudio::initialize().unwrap();
-
-        let stream = portaudio::stream::Stream::open_default(
-                0, 2, 44100.0,
-                portaudio::stream::FRAMES_PER_BUFFER_UNSPECIFIED,
-                None
-        ).unwrap();
-
-        PortAudioSink(stream)
+        PortAudioSink(None)
     }
 }
 
 impl <'a> Sink for PortAudioSink<'a> {
-    fn start(&self) -> io::Result<()> {
-        self.0.start().unwrap();
+    fn start(&mut self) -> io::Result<()> {
+        if self.0.is_some() {
+        } else {
+            self.0 = Some(portaudio::stream::Stream::open_default(
+                0, 2, 44100.0,
+                portaudio::stream::FRAMES_PER_BUFFER_UNSPECIFIED,
+                None
+            ).unwrap());
+        }
+
+        self.0.as_mut().unwrap().start().unwrap();
         Ok(())
     }
-    fn stop(&self) -> io::Result<()> {
-        self.0.stop().unwrap();
+    fn stop(&mut self) -> io::Result<()> {
+        self.0.as_mut().unwrap().stop().unwrap();
+        self.0 = None;
         Ok(())
     }
-    fn write(&self, data: &[i16]) -> io::Result<()> {
-        match self.0.write(&data) {
+    fn write(&mut self, data: &[i16]) -> io::Result<()> {
+        match self.0.as_mut().unwrap().write(&data) {
             Ok(_) => (),
             Err(portaudio::PaError::OutputUnderflowed) => error!("PortAudio write underflow"),
             Err(e) => panic!("PA Error {}", e),
